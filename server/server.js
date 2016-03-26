53f42b6ceb0d4092218259ed592c8f8e188af31c
Daniel Dawson-Brown <danielddb@hotmail.co.uk>
2016-03-26 15:35:08 +0000

    Added port variable

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 // Strips /server folder from __dirname
 var projectRoot = function() {
@@ -16,6 +17,6 @@ app.get('*', function(req, res) {
     res.sendFile(projectRoot + '/app/index.html');
 });
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Fluxy listening on port 3000!');
+app.listen(port, function () {
+    console.log('Fluxy listening on port ' + port + '!');
 });
